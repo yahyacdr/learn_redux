f03f261b42b951a011092a157db5263638a9050d
yahyacdr <yahya.zahouani7@gmail.com>
2024-04-14 10:12:07 +0100

    Initial state Use Classic Redux

---
+++ src/store.js
@@ -0,0 +1,40 @@
+import { createStore } from "redux";
+
+const initialAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+const initialCustomer = {
+  fullName: "",
+  nationaID: "",
+  createdAt: "",
+};
+
+function reducer(state = initialAccount, action) {
+  switch (action.type) {
+    case "action/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "action/withdrawal":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      // LATER
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "action/deposit", payload: 500 });
+console.log(store.getState());
